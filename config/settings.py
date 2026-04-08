580e9c791f4eb3fffb2c2211f3802dc21be0be89
rightpleft <rightpleft@gmail.com>
2026-04-08 13:14:00 +0900

    fix broken requirements.txt encoding

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
+
     'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
