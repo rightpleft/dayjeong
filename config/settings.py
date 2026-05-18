c2402f34e3b7d68bb4727eb9d04cd203d443ac7d
rightpleft <rightpleft@gmail.com>
2026-05-18 10:22:39 +0900

    fix admin duplicate

--- config/settings.py
+++ config/settings.py
@@ -35,8 +35,6 @@ INSTALLED_APPS = [
     #'colorfield',
     'jazzmin',
     'django.contrib.admin',
-
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
