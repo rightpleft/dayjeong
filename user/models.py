be406ea0d995077b0241c6df77196d0c5992f828
rightpleft <rightpleft@gmail.com>
2026-05-18 15:35:21 +0900

    친구요청 오류 수정

--- user/models.py
+++ user/models.py
@@ -31,3 +31,12 @@ class ScheduleRequest(models.Model):
         ('accepted', '수락'),
         ('rejected', '거절')
     ], default='pending')
+
+#친구 요청
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_user} → {self.to_user}"
